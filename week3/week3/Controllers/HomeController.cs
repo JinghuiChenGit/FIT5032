d167543ea5fee910e3d6cb5bf26e8a17edfd0ccd
JinghuiChen <jche0172@student.monash.edu>
2019-08-12 11:50:05 +1000

    week03 version 1.0

---
+++ week3/week3/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using week3.HelloWorld;
+using week3.Exercise;
+
+namespace week3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            Hello hello = new Hello();
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Message2 = hello.GetHello();
+            ExampleDictionary exampleDictionary = new ExampleDictionary();
+            exampleDictionary.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
